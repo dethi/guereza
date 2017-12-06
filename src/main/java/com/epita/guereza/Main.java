3ab6758af78f54b214905387e3fdf7afcc49b357
Thibault Deutsch <thibault.deutsch@gmail.com>
2017-12-06 11:57:51 +0100

    Add logger example

--- src/main/java/com/epita/guereza/Main.java
+++ src/main/java/com/epita/guereza/Main.java
@@ -1,7 +1,13 @@
 package com.epita.guereza;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.out.println("hello");
+        logger.debug("test");
     }
 }
