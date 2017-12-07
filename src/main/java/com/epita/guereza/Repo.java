df291dae61e8239e975c2fc6a5879496065cab41
Florent Youinou <youino_f@epita.fr>
2017-12-07 12:57:39 +0100

    misc: add comments on interfaces

--- src/main/java/com/epita/guereza/Repo.java
+++ src/main/java/com/epita/guereza/Repo.java
@@ -1,7 +1,17 @@
 package com.epita.guereza;
 
 public interface Repo {
+    /**
+     * Add urls to the repo
+     *
+     * @param urls The urls to add to the repo
+     */
     void store(String[] urls);
 
+    /**
+     * Return the next url to crawl
+     *
+     * @return The Url
+     */
     String nextUrl();
 }
--- src/main/java/com/epita/guereza/domain/Indexer.java
+++ src/main/java/com/epita/guereza/domain/Indexer.java
@@ -5,9 +5,29 @@ import java.util.Map;
 
 public interface Indexer {
 
+    /**
+     * Get a document by url
+     *
+     * @param url The url to index
+     * @return The result document
+     */
     Document index(final String url);
 
+    /**
+     * Add a document to the index
+     *
+     * @param index The index
+     * @param doc The doc to store
+     */
+    void publish(final Index index, final Document doc);
+
+    /**
+     * Search a query in a list of document
+     *
+     * @param docs The list of documents
+     * @param query The string query to search
+     * @return The document map result
+     */
     Map<Document, Double> search(final List<Document> docs, final String query);
 
-    void publish(final Index index, final Document doc);
 }
